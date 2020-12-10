a8709c8059b457c4db14d9ad552e696f21bfa3cc
nkgawade <gawade@gmail.com>
2020-12-10 20:11:19 +0530

    Accounts navigation fix

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -22,7 +22,7 @@ namespace FuzzBrain.Controllers
                 if (isValidUser)
                 {
                     FormsAuthentication.SetAuthCookie(model.UserName, false);
-                    return RedirectToAction("GetChallenge", "Challenges");
+                    return RedirectToAction("Index", "Challenges");
                 }
 
                 ModelState.AddModelError(string.Empty, "Invalid Username or Password");
